d3f9e80abc9770e8718d8e9fd16beb64b2f51273
Henry Blanchette <henry@monkeyface.com>
2025-05-08 15:56:19 -0400

    why do categories and feeds not update?

--- src/Summaries.tsx
+++ src/Summaries.tsx
@@ -18,6 +18,18 @@ export default function Summaries(props: {}) {
     new Set(),
   );
 
+  const [feedTitles, modify_feedTitles] = useState<Set<string>>(new Set());
+  const [feedsActive, modify_feedTitlesActive] = useState<Set<string>>(
+    new Set(),
+  );
+
+  function isActiveItem(item: SummarizedFeedItem): boolean {
+    return (
+      categoriesActive.intersection(new Set(item.categories ?? [])).size > 0 ||
+      feedsActive.intersection(new Set(item.feed_title)).size > 0
+    );
+  }
+
   function render_category(category: string) {
     const active = categoriesActive.has(category);
     return (
@@ -26,17 +38,40 @@ export default function Summaries(props: {}) {
         className={`category ${categoriesActive.size === 0 ? "" : active ? "active" : "inactive"}`}
         onClick={() => {
           if (active) {
-            modify_categoriesActive(
+            modify_categoriesActive((categoriesActive) =>
               categoriesActive.difference(new Set([category])),
             );
           } else {
-            modify_categoriesActive(
+            modify_categoriesActive((categoriesActive) =>
               categoriesActive.union(new Set([category])),
             );
           }
         }}
       >
         {category}
+      </div>
+    );
+  }
+
+  function render_feed(feedTitle: string) {
+    const active = feedsActive.has(feedTitle);
+    return (
+      <div
+        key={feedTitle}
+        className={`feed ${feedsActive.size === 0 ? "" : active ? "active" : "inactive"}`}
+        onClick={() => {
+          if (active) {
+            modify_feedTitlesActive((feedsActive) => {
+              return feedsActive.difference(new Set([feedTitle]));
+            });
+          } else {
+            modify_feedTitlesActive((feedsActive) => {
+              return feedsActive.union(new Set([feedTitle]));
+            });
+          }
+        }}
+      >
+        {feedTitle}
       </div>
     );
   }
@@ -49,11 +84,15 @@ export default function Summaries(props: {}) {
         ).json();
         modify_items(items);
         modify_itemsAll(items);
-        const categories_strings = new Set(
-          items.flatMap((item) => item.categories ?? []),
+
+        modify_categories(
+          new Set(items.flatMap((item) => item.categories ?? [])),
         );
-        modify_categories(categories_strings);
         modify_categoriesActive(new Set());
+
+        modify_feedTitles(new Set(items.flatMap((item) => item.feed_title)));
+        modify_feedTitlesActive(new Set());
+
         modify_status(undefined);
       } catch (e: any) {
         modify_status(`error fetching feed item catalogue: ${e.toString()}`);
@@ -62,64 +101,72 @@ export default function Summaries(props: {}) {
   }, []);
 
   useEffect(() => {
-    if (categoriesActive.size === 0) {
+    if (categoriesActive.size === 0 && feedsActive.size === 0) {
       modify_items(itemsAll);
     } else {
-      modify_items(
-        itemsAll.filter((item) => {
-          return (
-            categoriesActive.intersection(new Set(item.categories ?? [])).size >
-            0
-          );
-        }),
-      );
+      modify_items(itemsAll.filter(isActiveItem));
     }
-  }, [categoriesActive]);
+  }, [categoriesActive, feedsActive]);
+
+  console.log("feedsActive", JSON.stringify(feedsActive));
+  console.log("categoriesActive", JSON.stringify(categoriesActive));
 
   return (
     <div className="Summaries panel">
       <div className="title">Summaries</div>
       {status === undefined ? <></> : <Status content={status} />}
-      <div className="body">
-        <div className="categories-filler"></div>
-        <div className="categories">
-          {do_(() => {
-            const elements: ReactNode[] = [];
-            var i = 0;
-            for (const category of Array(...categories).toSorted()) {
-              const active = categoriesActive.has(category);
-              elements.push(render_category(category));
-              i++;
-            }
-            return elements;
-          })}
-        </div>
-        <div className="items">
-          {items.map((item, i) => (
-            <>
-              <hr key={i} />
-              <div
-                key={`${item.title} (published ${item.pubDate})`}
-                className="item"
-              >
-                <div className="header">
-                  <a className="title" href={item.link}>
-                    {item.title}
-                  </a>
+      <div className="section">Categories</div>
+      <div className="categories">
+        {do_(() => {
+          const elements: ReactNode[] = [];
+          var i = 0;
+          for (const category of Array(...categories).toSorted()) {
+            const active = categoriesActive.has(category);
+            elements.push(render_category(category));
+            i++;
+          }
+          return elements;
+        })}
+      </div>
+      <div className="section">Feeds</div>
+      <div className="feeds">
+        {do_(() => {
+          const elements: ReactNode[] = [];
+          var i = 0;
+          for (const feedTitle of Array(...feedTitles).toSorted()) {
+            const active = feedsActive.has(feedTitle);
+            elements.push(render_feed(feedTitle));
+            i++;
+          }
+          return elements;
+        })}
+      </div>
+      <div className="section">Items</div>
+      <div className="items">
+        {items.map((item, i) => (
+          <>
+            <hr key={i} />
+            <div
+              key={`${item.title} (published ${item.pubDate})`}
+              className="item"
+            >
+              <div className="header">
+                <a className="title" href={item.link}>
+                  {item.title}
+                </a>
 
-                  <a className="feed_title" href={item.feed_url}>
-                    {item.feed_title}
-                  </a>
-                </div>
-                <div className="pubDate">{item.pubDate}</div>
-                <div className="categories">
-                  {(item.categories ?? []).map(render_category)}
-                </div>
-                <div className="summary">{item.summary}</div>
+                <a className="feed_title" href={item.feed_url}>
+                  {item.feed_title}
+                </a>
               </div>
-            </>
-          ))}
-        </div>
+              <div className="pubDate">{item.pubDate}</div>
+              <div className="categories">
+                {(item.categories ?? []).map(render_category)}
+              </div>
+              <div className="summary">{item.summary}</div>
+            </div>
+          </>
+        ))}
       </div>
     </div>
   );
